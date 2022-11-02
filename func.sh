2c05abea8e9a02efd35287739b4288c25baad54a
chaniqure <1255861908@qq.com>
2022-11-02 17:22:04 +0800

    Update func.sh

--- func.sh
+++ func.sh
@@ -117,7 +117,9 @@ function dockerInstall(){
 }
 function dockerComposeInstall(){
     mustRootUser
-    curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose & chmod +x /usr/local/bin/docker-compose
+    apt-get update
+    apt-get install docker-compose-plugin
+    # curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose & chmod +x /usr/local/bin/docker-compose
     # curl -sSL https://get.daocloud.io/docker | sh
 }
 # 更新docker仓库
